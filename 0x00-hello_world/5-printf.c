656f54fcb215108e4c842e0c84d2d09d94b00d97
Zeusarc <zulka4real@gmail.com>
2023-07-13 10:19:05 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main - Short description, single line
+*
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
