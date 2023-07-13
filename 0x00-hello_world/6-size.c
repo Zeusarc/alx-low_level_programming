2f2777f70db06fd4cd4dd3ce6babad2078709b35
Zeusarc <zulka4real@gmail.com>
2023-07-13 10:47:40 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - Short description, single line
+*
+ * Return: Description of the returned value
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of a int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sixeof(long int));
+	printf("Size of a long long int: %d byte(s)", sixeof(long long char));
+	printf("Size of a float: %d byte(s)", sixeof(float));
+	return (0);
+}
