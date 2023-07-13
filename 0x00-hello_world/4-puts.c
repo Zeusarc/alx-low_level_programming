3041dee97171ce14fdcce2b411de533d6f5f1202
Zeusarc <zulka4real@gmail.com>
2023-07-13 10:08:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+/**
+* main - Short description, single line
+*
+ * Return: Description of the returned value
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
