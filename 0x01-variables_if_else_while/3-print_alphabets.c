1922c77c204218475afdfffcf0b239d61bbd560a
Zeusarc <zulka4real@gmail.com>
2023-07-14 12:20:40 -0700

    mm

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Short description,
+*
+* Return: Description of the returned value
+ */
+int main(void)
+{
+	int a = 97;
+	int b = 65;
+
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	while (a <= 90)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
