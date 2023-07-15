c8fa1b83b72d6ccfe0472af546cbea5b78dcbf32
Zeusarc <zulka4real@gmail.com>
2023-07-14 18:35:57 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Short description,
+*
+* Return: Description of the returned value
+ */
+int main(void)
+{
+	int a = 122;
+
+	while (a >= 97)
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
