afac0069f81b71268df60ef2d5095c6aae69d84c
Zeusarc <zulka4real@gmail.com>
2023-07-14 19:19:42 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Short description,
+*
+* Return: Description of the returned value
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+		putchar(',');
+		putchar(' ';)
+	}
+	putchar('\n');
+	return (0);
+}
