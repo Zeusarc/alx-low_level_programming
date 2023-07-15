16768156a87334bba251c775b3621d6a14235e74
Zeusarc <zulka4real@gmail.com>
2023-07-14 18:24:38 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Short description,
+*
+* Return: Description of the returned value
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a + 48);
+	}
+	putchar('\n');
+	return (0);
+}
