cb2744fd7d8e6667b0b2a02340f3bf6611c88d80
Zeusarc <zulka4real@gmail.com>
2023-07-18 15:35:18 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_sign - Short description,
+*
+* @n:  gggg
+* Return: Description of the returned value
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(00);
+		return (0);
+	}
+
+	_putchar('\n');
+}
