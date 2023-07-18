30761a4822d24011fb1a5456b310317801acd21d
Zeusarc <zulka4real@gmail.com>
2023-07-18 14:58:12 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,12 @@
 /**
 * _islower - Short description,
 *
+* @c:  gggg
 * Return: Description of the returned value
 */
 int _islower(int c)
 {
-	if (c <=97 && c>=122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
