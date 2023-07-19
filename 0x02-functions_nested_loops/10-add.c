eba4a3adaf531a4c06723557546b763b13cb6500
Zeusarc <zulka4real@gmail.com>
2023-07-18 20:36:43 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - Short description,
+*
+* @a: integer a
+*
+* @b: integer b
+*
+* Return: Description of the returned value
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
