2a924a36f340d7d4e25b494a2aab5f0a0a946113
Zeusarc <zulka4real@gmail.com>
2023-07-18 18:49:13 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _abs - Short description,
+*
+* @c:  gggg
+* Return: Description of the returned value
+*/
+int _abs(int i)
+{
+	if (i >= 0 )
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+	_putchar('\n');
+}
