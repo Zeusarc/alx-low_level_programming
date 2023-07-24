f64930b459ca14ac2b9e86e82659b866997903e1
Zeusarc <zulka4real@gmail.com>
2023-07-24 16:14:59 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-swap.c -o 1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * swap_int - take in two variables and swap them
+ * @a: stores adress of b after swap
+ * @b: stores adress of a after swap
+ * Return: always return 0
+ */
+
+void swap_int(int *a, int *b)
+{
+int swap;
+swap = *b;
+*b = *a;
+*a = swap;
+}
