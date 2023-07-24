116f094e718fd05872bc384578e059bb11388c14
Zeusarc <zulka4real@gmail.com>
2023-07-24 16:24:45 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
-/*
+/**
  * swap_int - take in two variables and swap them
  * @a: stores adress of b after swap
  * @b: stores adress of a after swap
- * @swap: temporary stores value to be swapped
  * Return: always return 0
  */
 
 void swap_int(int *a, int *b)
 {
-int swap;
-swap = *b;
+int hold;
+hold = *b;
 *b = *a;
-*a = swap;
+*a = hold;
 }
