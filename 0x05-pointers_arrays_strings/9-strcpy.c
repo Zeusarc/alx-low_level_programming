f7a485264b7a38e43dfe324df183dc41ac5faba9
Zeusarc <zulka4real@gmail.com>
2023-07-24 21:19:53 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int z = 1;
+	int z = -1;
 
 	do {
 		z++;
 		dest[z] = src[z];
-	} while (src[z] != ('\0');
+	} while (src[z] != '\0');
 
 	return (dest);
 }
