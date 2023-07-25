ba9a565523945353ede1428345d6abc0838384c3
Zeusarc <zulka4real@gmail.com>
2023-07-24 20:50:05 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,10 @@ void puts_half(char *str)
 	int full_string, half_string;
 
 	full_string = 0;
-	while (str[full_string] != '0')
+	while (str[full_string] != '\0')
 		full_string++;
 
-	half_string = 0;
+	half_string = full_string / 2;
 
 	if (full_string % 2 == 1)
 		half_string++;
