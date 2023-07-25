bd21c33b10b532a2b0f4566726e395db7945ea5e
Zeusarc <zulka4real@gmail.com>
2023-07-24 20:00:37 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - gkjfdmsaSDFGH
+ * @str: hgfdvcderbtnyunhg
+ * Return: vbhgjyrtbgvfd cx
+ */
+
+void puts2(char *n)
+{
+	int z = 0;
+
+	while (str[z] != '\0')
+	{
+		if (z % 2 == 0)
+		{
+			_putschar(str[z];)
+		}
+		z++;
+	}
+	_putschar('\n');
+}
