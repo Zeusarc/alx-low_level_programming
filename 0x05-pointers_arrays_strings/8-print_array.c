08f1fa37acad2f8054d428a320794235f3de4afa
Zeusarc <zulka4real@gmail.com>
2023-07-24 20:59:24 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_array - bjfboiv sk 
+ * @a: ehtrnaz
+ * @n: eaverbznb/rmrrr
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int index_of_array;
+
+	for (index_of_array = 0; index_of_array < n; index_of_array++)
+	{
+		printf("%d", a[index_of_array]);
+		if (index_of_array != (n - 1))
+		{
+			printf(",");
+		}
+	}
+	putchar('\n');
+}
