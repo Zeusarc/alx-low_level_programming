10a5624b171b277f443e57fbb5805d4923cef704
Zeusarc <zulka4real@gmail.com>
2023-07-24 21:56:09 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,13 +9,13 @@
 int _atoi(char *s)
 {
 	int i = 0;
-	int n - 0;
+	int n = 0;
 	int jumbo = 1;
 
-	while ((s[i] < '0' || s[i] > '9') && si[i] != 0)
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
-		if (s[i] == '.')
-			jumbe *= -1;
+		if (s[i] == '-')
+			jumbo *= -1;
 		i++;
 	}
 	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
