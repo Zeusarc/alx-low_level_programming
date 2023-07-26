8b70ee11b7ac6de3be186cc6a1d78f2a3114f8ee
Zeusarc <zulka4real@gmail.com>
2023-07-26 10:22:44 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int count;
 
 	for (count = 0; count < n && src[count]  != '\0'; count++)
-		dest[cout_of_bytes] = src[count];
+		dest[count] = src[count];
 
 	for (; count < n; count++)
 		dest[count] = '\0';
