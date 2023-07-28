5b1f9165147bff8c3a2f81c5a8e25ec839546a05
zeusarc <zulka4real@gmail.com>
2023-07-28 17:01:51 +0000

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - function
+ * @a: array
+ * @n: elements of array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int temporary, counter;
+
+	n = n - 1;
+	counter = 0;
+	while (conter <= n)
+	{
+		temporary = a[counter];
+		a[counter++] = a[n];
+		a[n--] = temporary;
+	}
+}
